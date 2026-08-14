32124b79356ec69a72062ada3145dcb272169d3c
Justin Kim <justin.joy.9to5@gmail.com>
2026-08-14 11:48:22 +0900

    refactor(columnar): extract checked arithmetic

--- wirelog/columnar/ops.c
+++ wirelog/columnar/ops.c
@@ -372,123 +372,6 @@ filt_pop(filt_stack_t *s)
     return s->top != 0 ? s->vals[--s->top] : 0;
 }
 
-static inline uint64_t
-wl_columnar_ops_abs_u64(int64_t v)
-{
-    return v < 0 ? (uint64_t)0 - (uint64_t)v : (uint64_t)v;
-}
-
-/* Use compiler builtins where available; otherwise use explicit range checks. */
-#if defined(__GNUC__) || defined(__clang__)
-#define WL_COLUMNAR_OPS_HAVE_INT64_OVERFLOW_BUILTIN 1
-#elif defined(__has_builtin)
-#if __has_builtin(__builtin_add_overflow) \
-    && __has_builtin(__builtin_sub_overflow) \
-    && __has_builtin(__builtin_mul_overflow)
-#define WL_COLUMNAR_OPS_HAVE_INT64_OVERFLOW_BUILTIN 1
-#endif
-#endif
-
-/* Checked int64 arithmetic helpers shared by both slow and compiled evaluators. */
-static int
-wl_columnar_ops_checked_add_int64(int64_t a, int64_t b, int64_t *out)
-{
-#if defined(WL_COLUMNAR_OPS_HAVE_INT64_OVERFLOW_BUILTIN)
-    return __builtin_add_overflow(a, b, out) ? ERANGE : 0;
-#else
-    if (b > 0) {
-        if (a > INT64_MAX - b)
-            return ERANGE;
-    } else {
-        if (a < INT64_MIN - b)
-            return ERANGE;
-    }
-    *out = a + b;
-    return 0;
-#endif
-}
-
-static int
-wl_columnar_ops_checked_sub_int64(int64_t a, int64_t b, int64_t *out)
-{
-#if defined(WL_COLUMNAR_OPS_HAVE_INT64_OVERFLOW_BUILTIN)
-    return __builtin_sub_overflow(a, b, out) ? ERANGE : 0;
-#else
-    if (b > 0) {
-        if (a < INT64_MIN + b)
-            return ERANGE;
-    } else {
-        if (a > INT64_MAX + b)
-            return ERANGE;
-    }
-    *out = a - b;
-    return 0;
-#endif
-}
-
-static int
-wl_columnar_ops_checked_mul_int64(int64_t a, int64_t b, int64_t *out)
-{
-#if defined(WL_COLUMNAR_OPS_HAVE_INT64_OVERFLOW_BUILTIN)
-    return __builtin_mul_overflow(a, b, out) ? ERANGE : 0;
-#else
-    if (a == 0 || b == 0) {
-        *out = 0;
-        return 0;
-    }
-    if ((a == -1 && b == INT64_MIN) || (b == -1 && a == INT64_MIN))
-        return ERANGE;
-
-    uint64_t ua = wl_columnar_ops_abs_u64(a);
-    uint64_t ub = wl_columnar_ops_abs_u64(b);
-    if ((a < 0) == (b < 0)) {
-        if (ua > (uint64_t)INT64_MAX / ub)
-            return ERANGE;
-    } else {
-        if (ua > ((uint64_t)INT64_MAX + 1ULL) / ub)
-            return ERANGE;
-    }
-    *out = a * b;
-    return 0;
-#endif
-}
-
-static int
-wl_columnar_ops_checked_div_int64(int64_t a, int64_t b, int64_t *out)
-{
-    if (b == 0 || (a == INT64_MIN && b == -1))
-        return ERANGE;
-    *out = a / b;
-    return 0;
-}
-
-static int
-wl_columnar_ops_checked_mod_int64(int64_t a, int64_t b, int64_t *out)
-{
-    if (b == 0 || (a == INT64_MIN && b == -1))
-        return ERANGE;
-    *out = a % b;
-    return 0;
-}
-
-static int
-wl_columnar_ops_checked_shl_int64(int64_t a, int64_t b, int64_t *out)
-{
-    if (b < 0 || b >= 64)
-        return ERANGE;
-    *out = (int64_t)((uint64_t)a << (uint32_t)b);
-    return 0;
-}
-
-static int
-wl_columnar_ops_checked_shr_int64(int64_t a, int64_t b, int64_t *out)
-{
-    if (b < 0 || b >= 64)
-        return ERANGE;
-    *out = a >> (uint32_t)b;
-    return 0;
-}
-
 /*
  * col_eval_expr_run:
  * Core postfix expression evaluator. Runs the bytecode against a row and
@@ -574,7 +457,7 @@ col_eval_expr_run(const uint8_t *buf, uint32_t size, const int64_t *row,
         case WL_PLAN_EXPR_ARITH_ADD: {
             int64_t b = filt_pop(&s), a = filt_pop(&s);
             int64_t v;
-            if (wl_columnar_ops_checked_add_int64(a, b, &v) != 0)
+            if (wl_columnar_arithmetic_checked_add_int64(a, b, &v) != 0)
                 goto bad;
             filt_push(&s, v);
             break;
@@ -582,7 +465,7 @@ col_eval_expr_run(const uint8_t *buf, uint32_t size, const int64_t *row,
         case WL_PLAN_EXPR_ARITH_SUB: {
             int64_t b = filt_pop(&s), a = filt_pop(&s);
             int64_t v;
-            if (wl_columnar_ops_checked_sub_int64(a, b, &v) != 0)
+            if (wl_columnar_arithmetic_checked_sub_int64(a, b, &v) != 0)
                 goto bad;
             filt_push(&s, v);
             break;
@@ -590,7 +473,7 @@ col_eval_expr_run(const uint8_t *buf, uint32_t size, const int64_t *row,
         case WL_PLAN_EXPR_ARITH_MUL: {
             int64_t b = filt_pop(&s), a = filt_pop(&s);
             int64_t v;
-            if (wl_columnar_ops_checked_mul_int64(a, b, &v) != 0)
+            if (wl_columnar_arithmetic_checked_mul_int64(a, b, &v) != 0)
                 goto bad;
             filt_push(&s, v);
             break;
@@ -598,7 +481,7 @@ col_eval_expr_run(const uint8_t *buf, uint32_t size, const int64_t *row,
         case WL_PLAN_EXPR_ARITH_DIV: {
             int64_t b = filt_pop(&s), a = filt_pop(&s);
             int64_t v;
-            if (wl_columnar_ops_checked_div_int64(a, b, &v) != 0)
+            if (wl_columnar_arithmetic_checked_div_int64(a, b, &v) != 0)
                 goto bad;
             filt_push(&s, v);
             break;
@@ -606,7 +489,7 @@ col_eval_expr_run(const uint8_t *buf, uint32_t size, const int64_t *row,
         case WL_PLAN_EXPR_ARITH_MOD: {
             int64_t b = filt_pop(&s), a = filt_pop(&s);
             int64_t v;
-            if (wl_columnar_ops_checked_mod_int64(a, b, &v) != 0)
+            if (wl_columnar_arithmetic_checked_mod_int64(a, b, &v) != 0)
                 goto bad;
             filt_push(&s, v);
             break;
@@ -634,7 +517,7 @@ col_eval_expr_run(const uint8_t *buf, uint32_t size, const int64_t *row,
         case WL_PLAN_EXPR_ARITH_SHL: {
             int64_t b = filt_pop(&s), a = filt_pop(&s);
             int64_t v;
-            if (wl_columnar_ops_checked_shl_int64(a, b, &v) != 0)
+            if (wl_columnar_arithmetic_checked_shl_int64(a, b, &v) != 0)
                 goto bad;
             filt_push(&s, v);
             break;
@@ -642,7 +525,7 @@ col_eval_expr_run(const uint8_t *buf, uint32_t size, const int64_t *row,
         case WL_PLAN_EXPR_ARITH_SHR: {
             int64_t b = filt_pop(&s), a = filt_pop(&s);
             int64_t v;
-            if (wl_columnar_ops_checked_shr_int64(a, b, &v) != 0)
+            if (wl_columnar_arithmetic_checked_shr_int64(a, b, &v) != 0)
                 goto bad;
             filt_push(&s, v);
             break;
@@ -1346,7 +1229,7 @@ col_eval_expr_compiled(const col_expr_compiled_t *c, const int64_t *row,
         case WL_PLAN_EXPR_ARITH_ADD: {
             int64_t b = filt_pop(&s), a = filt_pop(&s);
             int64_t v;
-            if (wl_columnar_ops_checked_add_int64(a, b, &v) != 0) {
+            if (wl_columnar_arithmetic_checked_add_int64(a, b, &v) != 0) {
                 *out_val = 0;
                 return 1;
             }
@@ -1356,7 +1239,7 @@ col_eval_expr_compiled(const col_expr_compiled_t *c, const int64_t *row,
         case WL_PLAN_EXPR_ARITH_SUB: {
             int64_t b = filt_pop(&s), a = filt_pop(&s);
             int64_t v;
-            if (wl_columnar_ops_checked_sub_int64(a, b, &v) != 0) {
+            if (wl_columnar_arithmetic_checked_sub_int64(a, b, &v) != 0) {
                 *out_val = 0;
                 return 1;
             }
@@ -1366,7 +1249,7 @@ col_eval_expr_compiled(const col_expr_compiled_t *c, const int64_t *row,
         case WL_PLAN_EXPR_ARITH_MUL: {
             int64_t b = filt_pop(&s), a = filt_pop(&s);
             int64_t v;
-            if (wl_columnar_ops_checked_mul_int64(a, b, &v) != 0) {
+            if (wl_columnar_arithmetic_checked_mul_int64(a, b, &v) != 0) {
                 *out_val = 0;
                 return 1;
             }
@@ -1376,7 +1259,7 @@ col_eval_expr_compiled(const col_expr_compiled_t *c, const int64_t *row,
         case WL_PLAN_EXPR_ARITH_DIV: {
             int64_t b = filt_pop(&s), a = filt_pop(&s);
             int64_t v;
-            if (wl_columnar_ops_checked_div_int64(a, b, &v) != 0) {
+            if (wl_columnar_arithmetic_checked_div_int64(a, b, &v) != 0) {
                 *out_val = 0;
                 return 1;
             }
@@ -1386,7 +1269,7 @@ col_eval_expr_compiled(const col_expr_compiled_t *c, const int64_t *row,
         case WL_PLAN_EXPR_ARITH_MOD: {
             int64_t b = filt_pop(&s), a = filt_pop(&s);
             int64_t v;
-            if (wl_columnar_ops_checked_mod_int64(a, b, &v) != 0) {
+            if (wl_columnar_arithmetic_checked_mod_int64(a, b, &v) != 0) {
                 *out_val = 0;
                 return 1;
             }
@@ -1416,7 +1299,7 @@ col_eval_expr_compiled(const col_expr_compiled_t *c, const int64_t *row,
         case WL_PLAN_EXPR_ARITH_SHL: {
             int64_t b = filt_pop(&s), a = filt_pop(&s);
             int64_t v;
-            if (wl_columnar_ops_checked_shl_int64(a, b, &v) != 0) {
+            if (wl_columnar_arithmetic_checked_shl_int64(a, b, &v) != 0) {
                 *out_val = 0;
                 return 1;
             }
@@ -1426,7 +1309,7 @@ col_eval_expr_compiled(const col_expr_compiled_t *c, const int64_t *row,
         case WL_PLAN_EXPR_ARITH_SHR: {
             int64_t b = filt_pop(&s), a = filt_pop(&s);
             int64_t v;
-            if (wl_columnar_ops_checked_shr_int64(a, b, &v) != 0) {
+            if (wl_columnar_arithmetic_checked_shr_int64(a, b, &v) != 0) {
                 *out_val = 0;
                 return 1;
             }
@@ -6939,7 +6822,7 @@ col_op_reduce(const wl_plan_op_t *op, eval_stack_t *stack,
             case WIRELOG_AGG_SUM:
             {
                 int64_t next;
-                if (wl_columnar_ops_checked_add_int64(cur, agg_val,
+                if (wl_columnar_arithmetic_checked_add_int64(cur, agg_val,
                     &next) != 0) {
                     col_row_buf_release(&row_rb);
                     col_expr_compiled_free(agg_ce);
